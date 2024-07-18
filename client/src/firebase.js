bf00b4cd83498577d2797e42286b3f6ef2b81a17
JUNPUYIN WEI NAME <naiqi666@gmail.com>
2024-07-18 01:12:28 -0400

    create and add oaUTH COMPONENT

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrJuTQAnxF-rAcpdmjdeY47UqLUgvNv80",
+  authDomain: "estate-3bb71.firebaseapp.com",
+  projectId: "estate-3bb71",
+  storageBucket: "estate-3bb71.appspot.com",
+  messagingSenderId: "433750432521",
+  appId: "1:433750432521:web:860dbf448ee9323b59d1d8",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
