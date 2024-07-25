a533f467cd2f2a17c089f0c4661ac03c3033ffae
JUNPUYIN WEI NAME <naiqi666@gmail.com>
2024-07-25 00:59:27 -0400

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -13,5 +13,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 
 export default router;
