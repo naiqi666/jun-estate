0ee1ccca2ceb7e70f311016916993354a66e5edf
JUNPUYIN WEI NAME <naiqi666@gmail.com>
2024-07-25 00:12:51 -0400

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
+  getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,5 +12,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
